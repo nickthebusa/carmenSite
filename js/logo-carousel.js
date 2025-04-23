2218898234d7d3cd5e2462ea02ec9093a71848e0
Nick Busa <nbusa1231@gmail.com>
2025-04-22 22:21:45 -0400

    finished home layout and carousel

--- js/logo-carousel.js
+++ js/logo-carousel.js
@@ -1,4 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const carouselEle = document.querySelector('.logo-carousel');
+  const carouselItems = document.querySelector('.carousel-items');
+  const prevBtn = document.querySelector('.carousel-prev');
+  const nextBtn = document.querySelector('.carousel-next');
 
+  prevBtn.addEventListener('click', () => {
+    const itemWidth = carouselItems.querySelector('*').clientWidth;
+    carouselItems.scrollBy({
+      left: -itemWidth,
+      behavior: 'smooth'
+    });
+  });
+
+  nextBtn.addEventListener('click', () => {
+    const itemWidth = carouselItems.querySelector('*').clientWidth;
+    carouselItems.scrollBy({
+      left: itemWidth,
+      behavior: 'smooth'
+    });
+  });
+
+  function updateButtonStates() {
+    prevBtn.disabled = carouselItems.scrollLeft <= 0;
+    nextBtn.disabled = carouselItems.scrollLeft + carouselItems.clientWidth >= carouselItems.scrollWidth;
+  }
+
+  carouselItems.addEventListener('scroll', updateButtonStates);
+  updateButtonStates();
 })
